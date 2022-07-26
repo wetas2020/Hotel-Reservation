d87e792d4bcb3b5803086920b3e6b43a227079f5
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-07-26 18:40:18 +0100

    add the routing for the pages

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import Room from './pages/Room';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
     return (
         <div>
-            <Home />
-            <Room />
-            <SingleRoom />
-            <Error />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/rooms/" element={<Room />} />
+                <Route path="/rooms/:slug" element={<SingleRoom />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
         </div>
     );
 }
