ab9f93d843077fa028495db650c507e66a94c8a0
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-07-26 18:07:18 +0100

    create  pages
     Home
    Room
    SingleRoom
    Error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
+import Home from './pages/Home';
+import Room from './pages/Room';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
 function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <Home />
+            <Room />
+            <SingleRoom />
+            <Error />
         </div>
     );
 }
