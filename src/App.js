0bb498eca6ab15abb67ee5befa9f122f1492aaa4
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-07-26 19:02:57 +0100

    add the navbar

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import Error from './pages/Error';
 
 import { Route, Routes } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+
 function App() {
     return (
         <div>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/rooms/" element={<Room />} />
