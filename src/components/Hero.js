c94ecd7d7a2483e292c0929bdba0d49977f249e3
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-07-27 17:51:37 +0100

    Add the Component Banner
    Add the Component Banner to home,rooms and error pages

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-export default function Hero({ childern, hero }) {
-    return <header className={hero}>{childern}</header>;
-}
+const Hero = ({ children, hero }) => {
+    return <header className={hero}>{children}</header>;
+};
+
+export default Hero;
 
 Hero.defaultProps = {
     hero: 'defaultHero'
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
-    return <Hero hero="defaultHero" />;
-};
-
-export default Home;
+export default function Home() {
+    return (
+        <Hero>
+            <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                <Link to="/rooms" className="btn-primary">
+                    our rooms
+                </Link>
+            </Banner>
+        </Hero>
+    );
+}
